cbc709dfdc96efe7eb7a6c680408fd841bd346a0
Carlos Ivanchuk <buhodev@proton.me>
2022-12-11 18:09:52 -0400

    feat: add new screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
+const { screens } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		screens: {
+			xs: '475px',
+			...screens
+		},
 		extend: {}
 	},
 
