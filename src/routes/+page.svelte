37de699135cffeef2f10a38afcb26fd52f9bba17
Carlos Ivanchuk <buhodev@proton.me>
2022-12-14 23:07:04 -0400

    feat: remove UltraGradientBackground on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
-<script>
+<script lang="ts">
 	import UltraGradientBackground from '$lib/components/UltraGradientBackground.svelte';
 	import Landing from '$lib/components/Landing.svelte';
+	import { onMount } from 'svelte';
+
+	let isMobile: Boolean;
+
+	onMount(() => {
+		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			isMobile = true
+		}
+	})
 </script>
 
-<UltraGradientBackground />
+{#if !isMobile}
+	<UltraGradientBackground />
+{/if}
 <Landing />
 
 <style>
